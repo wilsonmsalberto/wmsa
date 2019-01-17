15c220149116f4744dfb31bc70f2d09d1b18b177
Wilson Alberto <wilsonalberto@gmail.com>
2019-01-17 23:21:03 +0000

    fix: Update logo and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `wilson-alberto-website`,
+        short_name: `wmsa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#dc1718`,
+        theme_color: `#dc1718`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
